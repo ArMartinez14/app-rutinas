1adfa8f4f8370cd412a39265ea671ca216c4c73f
Ariel Martinez <arielmartinez@MacBook-Air-de-Ariel.local>
2025-06-01 16:22:01 -0400

    nuevo menu

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-import streamlit as st
-from ver_rutinas import ver_rutinas
-from crear_rutinas import crear_rutinas
+from menu import mostrar_menu
 
-st.set_page_config(page_title="Motion Center", layout="wide")
-
-st.sidebar.title("Menú Principal")
-opcion = st.sidebar.radio("Selecciona una opción:", ["Ver Rutinas", "Crear Rutinas"])
-
-if opcion == "Ver Rutinas":
-    ver_rutinas()
-elif opcion == "Crear Rutinas":
-    crear_rutinas()
+mostrar_menu()
--- menu.py
+++ menu.py
@@ -2,11 +2,13 @@ import streamlit as st
 from ver_rutinas import ver_rutinas
 from crear_rutinas import crear_rutinas
 
-st.sidebar.title("Menú Principal")
-opcion = st.sidebar.radio("Selecciona una opción:", ["Ver Rutinas", "Crear Rutinas"])
+def mostrar_menu():
+    st.set_page_config(page_title="Motion Center", layout="wide")
 
-if opcion == "Ver Rutinas":
-    ver_rutinas()
-elif opcion == "Crear Rutinas":
-    crear_rutinas()
+    st.sidebar.title("Menú Principal")
+    opcion = st.sidebar.radio("Selecciona una opción:", ["Ver Rutinas", "Crear Rutinas"])
 
+    if opcion == "Ver Rutinas":
+        ver_rutinas()
+    elif opcion == "Crear Rutinas":
+        crear_rutinas()
