889302a4489aaae216047993e8287a19b3b69db5
Ariel Martinez <arielmartinez@MacBook-Air-de-Ariel.local>
2025-06-01 11:55:23 -0400

    nuevomenu

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
+import streamlit as st
 from menu import mostrar_menu
 
-if __name__ == "__main__":
-    mostrar_menu()
+st.set_page_config(page_title="Motion Center", layout="wide")  # <-- aquí sí va
+mostrar_menu()
--- menu.py
+++ menu.py
@@ -3,8 +3,18 @@ from ver_rutinas import ver_rutinas
 from crear_rutinas import crear_rutinas
 
 def mostrar_menu():
-    st.set_page_config(page_title="Motion Center", layout="centered")
-    opcion = st.selectbox("📋 ¿Qué deseas hacer?", ["Ver Rutinas", "Crear Rutinas"])
+    # Eliminar espacio superior visualmente
+    st.markdown("""
+        <style>
+            .block-container {
+                padding-top: 1rem;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    st.sidebar.image("data:image/png;base64," + st.secrets["LOGO_BASE64"], use_column_width=True)
+
+    opcion = st.sidebar.selectbox("📋 ¿Qué deseas hacer?", ["Ver Rutinas", "Crear Rutinas"])
 
     if opcion == "Ver Rutinas":
         ver_rutinas()
