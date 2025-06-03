bab17b49445e0fbd78d9d8dc9af7424cdc3ec6dc
Ariel Martinez <arielmartinez@MacBook-Air-de-Ariel.local>
2025-06-03 08:54:18 -0400

    nueva duracion

--- app.py
+++ app.py
@@ -4,20 +4,24 @@ from crear_rutinas import crear_rutinas
 
 st.set_page_config(page_title="Motion Center", layout="wide")
 
+# Inicializar el estado de la sesión si no existe
+if 'opcion' not in st.session_state:
+    st.session_state.opcion = None
+
 # Título del sidebar
 st.sidebar.title("Menú principal")
 
 # Botones como menú lateral
 if st.sidebar.button("Ver Rutinas"):
-    st.session_state['opcion'] = "Ver Rutinas"
+    st.session_state.opcion = "Ver Rutinas"
 if st.sidebar.button("Crear Rutinas"):
-    st.session_state['opcion'] = "Crear Rutinas"
+    st.session_state.opcion = "Crear Rutinas"
 
 # Mostrar la opción seleccionada o el mensaje de bienvenida
-if 'opcion' in st.session_state:
-    if st.session_state['opcion'] == "Ver Rutinas":
+if st.session_state.opcion:
+    if st.session_state.opcion == "Ver Rutinas":
         ver_rutinas()
-    elif st.session_state['opcion'] == "Crear Rutinas":
+    elif st.session_state.opcion == "Crear Rutinas":
         crear_rutinas()
 else:
     st.markdown("""
