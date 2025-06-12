899c7d73bf441685432bb10fe7da275709469d96
Ariel Martinez <arielmartinez@Mac.lan>
2025-06-12 16:02:28 -0400

    volvi a lo anterior

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from ver_rutinas import ver_rutinas
 from crear_rutinas import crear_rutinas
 from evaluaciones import registrar_evaluacion
-from rutinas_admin import ver_editar_rutina
+from borrar_rutinas import borrar_rutinas  # 👈 IMPORTAR TU ARCHIVO
+from editar_rutinas import editar_rutinas
 
 st.set_page_config(page_title="Motion Center", layout="wide")
 
@@ -20,7 +21,8 @@ if st.sidebar.button("Crear Rutinas"):
     st.session_state.opcion = "Crear Rutinas"
 if st.sidebar.button("Evaluaciones"):
     st.session_state.opcion = "Evaluaciones"
-
+if st.sidebar.button("Borrar Rutinas"):
+    st.session_state.opcion = "Borrar Rutinas"
 if st.sidebar.button("Editar Rutinas"):
     st.session_state.opcion = "Editar Rutinas"
 
@@ -32,9 +34,10 @@ if st.session_state.opcion:
         crear_rutinas()
     elif st.session_state.opcion == "Evaluaciones":
         registrar_evaluacion()
-
+    elif st.session_state.opcion == "Borrar Rutinas":
+        borrar_rutinas()
     elif st.session_state.opcion == "Editar Rutinas":
-        ver_editar_rutina()
+        editar_rutinas()
 
 else:
     st.markdown("""
@@ -43,6 +46,4 @@ else:
             <h1>Bienvenido a Motion Center</h1>
             <p style='font-size:18px;'>Selecciona una opción del menú para comenzar</p>
         </div>
-    """, unsafe_allow_html=True)
-
-
+        """, unsafe_allow_html=True)
